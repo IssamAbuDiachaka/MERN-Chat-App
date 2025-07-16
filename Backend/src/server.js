c57d4decf9dfbf37b2810ec56aa2308820e41bfd
Issam Abu <issamabudiachaka@gmail.com>
2025-07-16 23:05:55 +0100

    updated

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import { connect } from 'mongoose';
+import messageRouter from './routes/message.route.js';
 import connectDB from './lib/db.js'
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
@@ -15,6 +15,7 @@ const app = express();
 app.use(express.json()); // Allow server to understand json data
 app.use(cors('*')); //Allow frontend to com wit backend
 app.use(cookieParser()); // Allows server to parse cookies
+// Connect to MongoDB
 
 
 app.get('/', (req, res) => {
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', authRouter); // Use authRouter for authentication routes
 
-
+app.use('/api/message', messageRouter); // Use messageRouter for messaging routes
 
 
 const PORT = process.env.PORT || 5000;
